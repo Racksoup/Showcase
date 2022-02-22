3d40bb852f2b24fbd70f3ba8112d401061fa5776
Racksoup <rack.connor1@gmail.com>
2022-02-22 09:28:07 -0500

    Showcase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='Background'></div>
+      <div className='Name'>Connor Rack</div>
+      <div className='PageFrame'>
+        <div className='Header'>
+          <p>Showcase</p>
+          <div className='HeaderLinks'>
+            <p>Blog</p>
+            <p>Github</p>
+          </div>
+        </div>
+        <div className='ShowcaseGrid'>
+          <p>1</p>
+          <p>2</p>
+          <p>3</p>
+          <div className='ShowcaseColumns'>
+            <div className='ShowcaseItem'></div>
+            <div className='ShowcaseItem'></div>
+            <div className='ShowcaseItem'></div>
+            <div className='ShowcaseItem'></div>
+            <div className='ShowcaseItem'></div>
+            <div className='ShowcaseItem'></div>
+          </div>
+          <div className='ShowcaseColumns ShowcaseColumnCenter'>
+            <div className='ShowcaseItem'></div>
+            <div className='ShowcaseItem'></div>
+            <div className='ShowcaseItem'></div>
+            <div className='ShowcaseItem'></div>
+            <div className='ShowcaseItem'></div>
+            <div className='ShowcaseItem'></div>
+          </div>
+          <div className='ShowcaseColumns'>
+            <div className='ShowcaseItem'></div>
+            <div className='ShowcaseItem'></div>
+            <div className='ShowcaseItem'></div>
+            <div className='ShowcaseItem'></div>
+            <div className='ShowcaseItem'></div>
+            <div className='ShowcaseItem'></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
